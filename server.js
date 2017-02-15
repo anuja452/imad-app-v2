f9b84d4e11d06f3589576b6f4123ad69684d0a38
anuja452 <anuja452@gmail.com>
2017-02-15 08:44:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,8 +102,8 @@ app.get('/', function (req, res) {
 
 var counter=0;
 app.get('/counter', function (req, res) {
-  counter=counter+1;
-  res.sendFile(counter.toString());
+  counter = counter + 1;
+  res.send(counter.toString());
 });
 
 
