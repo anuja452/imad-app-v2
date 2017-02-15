fdffdf9ea0fb286cedbeb0b873711f79365d6634
anuja452 <anuja452@gmail.com>
2017-02-15 10:02:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,3 @@ console.log('Loaded!');
 var element= document.getElementById("main-txt");
 element.innerHTML="new value";
 
-var img = document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginLeft='100px';
-};
